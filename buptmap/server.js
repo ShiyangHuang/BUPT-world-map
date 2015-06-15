88a7188c59a992416618a4b86259f092b95fdb97
Ubuntu <ubuntu@ip-172-31-19-216.us-west-2.compute.internal>
2015-06-15 02:50:35 +0000

    backend

--- buptmap/server.js
+++ buptmap/server.js
@@ -6,13 +6,13 @@ var http = require('http');
 var fs = require('fs');
 
 // MongoDB
-mongoose.connect('mongodb://localhost/rest_test');
+mongoose.connect('mongodb://localhost/buptmap1');
 
 // Express
 var app = express();
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
   next();
 });
